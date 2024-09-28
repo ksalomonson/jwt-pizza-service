c64facbb1d643695d848f4a45414254b494c12ea
ksalomonson <kennysalomonson@msn.com>
2024-09-28 13:38:22 -0600

    Added login tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,12 +2,14 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserId;
 let testUserAuthToken;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
 });
 
 test('login', async () => {
@@ -17,4 +19,37 @@ test('login', async () => {
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
+});
+
+test("update require auth", async () => {
+	const updateRes = await request(app)
+		.put("/api/auth/" + testUserId)
+		.send(testUser);
+	expect(updateRes.status).toBe(401);
+	expect(updateRes.body).toMatchObject({ message: "unauthorized" });
+});
+
+test("update", async () => {
+	const updateRes = await request(app)
+		.put("/api/auth/" + testUserId)
+		.set("Authorization", `Bearer ${testUserAuthToken}`)
+		.send(testUser);
+	expect(updateRes.status).toBe(200);
+	expect(updateRes.body.name).toBe(testUser.name);
+	expect(updateRes.body.email).toBe(testUser.email);
+});
+
+test("logout require auth", async () => {
+	const logoutRes = await request(app).delete("/api/auth").send(testUser);
+	expect(logoutRes.status).toBe(401);
+	expect(logoutRes.body).toMatchObject({ message: "unauthorized" });
+});
+
+test("logout", async () => {
+	const logoutRes = await request(app)
+		.delete("/api/auth")
+		.set("Authorization", `Bearer ${testUserAuthToken}`)
+		.send(testUser);
+	expect(logoutRes.status).toBe(200);
+	expect(logoutRes.body).toMatchObject({ message: "logout successful" });
 });
